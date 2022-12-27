9ab35d071ece4b1dd4edef67c64f84255fad3141
Вова Гнатюк <vololdymyr.gn@gmail.com>
2022-12-27 11:28:22 +0200

    lab_7

---
+++ lab_7/lab_7/lab_7.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+#define PI 3.14;
+#define CIRCLE_AREA(r) r * r * PI;
+
+    static float circleArea(int radius) {
+        return radius * radius * PI;
+    }
+
+    static void sayHello() {
+        cout << "Hello";
+    }
+
+    int main()
+    {
+        int r = 5;
+
+        cout << CIRCLE_AREA(r);
+        cout << endl;
+
+        cout << circleArea(r) << endl;
+
+        sayHello();
+    }
+
